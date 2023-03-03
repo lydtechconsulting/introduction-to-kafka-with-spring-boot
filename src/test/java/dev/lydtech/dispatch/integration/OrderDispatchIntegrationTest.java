558a4725079b46370416ffb8c789ef851ea474bf
rgolder1 <rgolder1@yahoo.com>
2023-03-03 14:09:18 +0000

    Skeleton integration test.

---
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -0,0 +1,21 @@
+package dev.lydtech.dispatch.integration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest()
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+@EmbeddedKafka(controlledShutdown = true)
+public class OrderDispatchIntegrationTest {
+
+    @Test
+    public void testOrderDispatchFlow() {
+
+    }
+}
