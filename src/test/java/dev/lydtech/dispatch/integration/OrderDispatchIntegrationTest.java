1e5c13831f41b06ee49470f7e67c82a1fedcfafd
rgolder1 <rgolder1@yahoo.com>
2023-04-25 13:19:34 +0100

    Test update.

--- src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -93,7 +93,7 @@ public class OrderDispatchIntegrationTest {
 
         @KafkaHandler
         void receiveOrderDispatched(@Header(KafkaHeaders.RECEIVED_KEY) String key, @Payload OrderDispatched payload) {
-            log.debug("Received OrderDispatched key: " + key + " - payload: : " + payload);
+            log.debug("Received OrderDispatched key: " + key + " - payload: " + payload);
             assertThat(key, notNullValue());
             assertThat(payload, notNullValue());
             orderDispatchedCounter.incrementAndGet();
@@ -101,7 +101,7 @@ public class OrderDispatchIntegrationTest {
 
         @KafkaHandler
         void receiveDispatchCompleted(@Header(KafkaHeaders.RECEIVED_KEY) String key, @Payload DispatchCompleted payload) {
-            log.debug("Received DispatchCompleted key: " + key + " - payload: : " + payload);
+            log.debug("Received DispatchCompleted key: " + key + " - payload: " + payload);
             assertThat(key, notNullValue());
             assertThat(payload, notNullValue());
             dispatchCompletedCounter.incrementAndGet();
@@ -109,7 +109,7 @@ public class OrderDispatchIntegrationTest {
 
         @KafkaHandler
         void receiveOrderCreatedDLT(@Header(KafkaHeaders.RECEIVED_KEY) String key, @Payload OrderCreated payload) {
-            log.debug("Received OrderCreated DLT key: " + key + " - payload: : " + payload);
+            log.debug("Received OrderCreated DLT key: " + key + " - payload: " + payload);
             assertThat(key, notNullValue());
             assertThat(payload, notNullValue());
             orderCreatedDLTCounter.incrementAndGet();
