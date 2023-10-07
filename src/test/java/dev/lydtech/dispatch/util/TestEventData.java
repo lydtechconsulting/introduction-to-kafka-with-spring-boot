152498055e6f08ebc26997da68b06c513cb998a4
panos-geladaris <panagiotis.geladaris@transferwise.com>
2023-10-07 21:03:10 +0100

    sending DispatchPreparing message from DispatchService

--- src/test/java/dev/lydtech/dispatch/util/TestEventData.java
+++ src/test/java/dev/lydtech/dispatch/util/TestEventData.java
@@ -1,14 +1,22 @@
 package dev.lydtech.dispatch.util;
 
+import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.dispatch.message.OrderCreated;
 
 import java.util.UUID;
 
 public class TestEventData {
+
     public static OrderCreated buildOrderCreatedEvent(UUID orderId, String item) {
         return OrderCreated.builder()
                 .orderId(orderId)
                 .item(item)
+                .build();
+    }
+
+    public static DispatchPreparing buildDispatchPreparing(UUID orderId) {
+        return DispatchPreparing.builder()
+                .orderId(orderId)
                 .build();
     }
 }
